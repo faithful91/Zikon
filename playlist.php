6d52731007599e2e8bf8bcbef42ba5fe0ffb06cd
Michael Traore <yecoux@gmail.com>
2015-02-03 22:06:21 +0100

    playlist

--- playlist.php
+++ playlist.php
@@ -14,7 +14,7 @@ catch (Exception $e)
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Genres</title>
+    <title>Playlist</title>
 
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
@@ -29,11 +29,340 @@ catch (Exception $e)
 
     <script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>
     <script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script>
+
+    <link href="jplayer/skin/blue.monday/css/jplayer.blue.monday.min.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="jplayer/jquery.jplayer.min.js"></script>
+    <script type="text/javascript" src="jplayer/add-on/jplayer.playlist.min.js"></script>
+    <?php session_start();?>
+    <script type="text/javascript">
+//<![CDATA[
+$(document).ready(function(){
+
+  var myPlaylist = new jPlayerPlaylist({
+    jPlayer: "#jquery_jplayer_N",
+    cssSelectorAncestor: "#jp_container_N"
+  }, [
+    <?php
+
+      try
+      {
+          // On se connecte à MySQL
+          $bdd = new PDO('mysql:host=localhost;dbname=zikon;charset=utf8', 'root', 'root');
+      }
+      catch(Exception $e)
+      {
+          // En cas d'erreur, on affiche un message et on arrête tout
+              die('Erreur : '.$e->getMessage());
+      }
+
+      // Si tout va bien, on peut continuer
+      $ids = join(',',$_SESSION['playlist']);  
+      // On récupère tout le contenu de la table jeux_video
+
+      $query = "SELECT titres_albums.id, albums.nom as anom, titres.nom as tnom , path , cover, genres.nom as gnom , artistes.nom as artnom FROM artistes,artistes_albums,titres_albums,albums,titres,genres WHERE titres_albums.titre = titres.id and titres_albums.album = albums.id and genres.id = titres.genre and artistes.id = artistes_albums.artiste and albums.id = artistes_albums.album and titres_albums.id IN(".implode(',',$_SESSION['playlist']).")";
+      $reponse = $bdd->query($query );
+
+      // On affiche chaque entrée une à une
+      $compteur = 0;
+      while ($donnees = $reponse->fetch())
+      {
+        if($compteur != 0){
+      ?>
+        ,
+      <?php  
+        } ?>
+        {
+            title:"<?php echo $donnees['tnom']; ?>",
+            artist:"<?php echo $donnees['artnom']; ?>",
+            mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+            poster: "<?php echo $donnees['cover']; ?>"
+        }
+      <?php 
+        $compteur++;
+      }
+      $reponse->closeCursor(); // Termine le traitement de la requête
+
+      ?>
+    ], {
+    playlistOptions: {
+      enableRemoveControls: true
+    },
+    swfPath: "./jplayer",
+    supplied: "webmv, ogv, m4v, oga, mp3",
+    useStateClassSkin: true,
+    autoBlur: false,
+    smoothPlayBar: true,
+    keyEnabled: true,
+    audioFullScreen: true
+  });
+
+  // Click handlers for jPlayerPlaylist method demo
+
+
+ 
+  // Audio mix playlist
+
+  $("#playlist-setPlaylist-audio-mix").click(function() {
+    myPlaylist.setPlaylist([
+      {
+        title:"Cro Magnon Man",
+        artist:"The Stark Palace",
+        mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+        oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+        poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+      },
+      {
+        title:"Your Face",
+        artist:"The Stark Palace",
+        mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+        oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg",
+        poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+      },
+      {
+        title:"Hidden",
+        artist:"Miaow",
+        free: true,
+        mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+        oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+        poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+      },
+      {
+        title:"Cyber Sonnet",
+        artist:"The Stark Palace",
+        mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+        oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg",
+        poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+      },
+      {
+        title:"Tempered Song",
+        artist:"Miaow",
+        mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+        oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg",
+        poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+      },
+      {
+        title:"Lentement",
+        artist:"Miaow",
+        mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+        oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg",
+        poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+      }
+    ]);
+  });
+
+
+  // The remove commands
+
+  $("#playlist-remove").click(function() {
+    myPlaylist.remove();
+  });
+
+  $("#playlist-remove--2").click(function() {
+    myPlaylist.remove(-2);
+  });
+  $("#playlist-remove--1").click(function() {
+    myPlaylist.remove(-1);
+  });
+  $("#playlist-remove-0").click(function() {
+    myPlaylist.remove(0);
+  });
+  $("#playlist-remove-1").click(function() {
+    myPlaylist.remove(1);
+  });
+  $("#playlist-remove-2").click(function() {
+    myPlaylist.remove(2);
+  });
+
+  // The shuffle commands
+
+  $("#playlist-shuffle").click(function() {
+    myPlaylist.shuffle();
+  });
+
+  $("#playlist-shuffle-false").click(function() {
+    myPlaylist.shuffle(false);
+  });
+  $("#playlist-shuffle-true").click(function() {
+    myPlaylist.shuffle(true);
+  });
+
+  // The select commands
+
+  $("#playlist-select--2").click(function() {
+    myPlaylist.select(-2);
+  });
+  $("#playlist-select--1").click(function() {
+    myPlaylist.select(-1);
+  });
+  $("#playlist-select-0").click(function() {
+    myPlaylist.select(0);
+  });
+  $("#playlist-select-1").click(function() {
+    myPlaylist.select(1);
+  });
+  $("#playlist-select-2").click(function() {
+    myPlaylist.select(2);
+  });
+
+  // The next/previous commands
+
+  $("#playlist-next").click(function() {
+    myPlaylist.next();
+  });
+  $("#playlist-previous").click(function() {
+    myPlaylist.previous();
+  });
+
+  // The play commands
+
+  $("#playlist-play").click(function() {
+    myPlaylist.play();
+  });
+
+  $("#playlist-play--2").click(function() {
+    myPlaylist.play(-2);
+  });
+  $("#playlist-play--1").click(function() {
+    myPlaylist.play(-1);
+  });
+  $("#playlist-play-0").click(function() {
+    myPlaylist.play(0);
+  });
+  $("#playlist-play-1").click(function() {
+    myPlaylist.play(1);
+  });
+  $("#playlist-play-2").click(function() {
+    myPlaylist.play(2);
+  });
+
+  // The pause command
+
+  $("#playlist-pause").click(function() {
+    myPlaylist.pause();
+  });
+
+  // Changing the playlist options
+
+  // Option: autoPlay
+
+  $("#playlist-option-autoPlay-true").click(function() {
+    myPlaylist.option("autoPlay", true);
+  });
+  $("#playlist-option-autoPlay-false").click(function() {
+    myPlaylist.option("autoPlay", false);
+  });
+
+  // Option: enableRemoveControls
+
+  $("#playlist-option-enableRemoveControls-true").click(function() {
+    myPlaylist.option("enableRemoveControls", true);
+  });
+  $("#playlist-option-enableRemoveControls-false").click(function() {
+    myPlaylist.option("enableRemoveControls", false);
+  });
+
+  // Option: displayTime
+
+  $("#playlist-option-displayTime-0").click(function() {
+    myPlaylist.option("displayTime", 0);
+  });
+  $("#playlist-option-displayTime-fast").click(function() {
+    myPlaylist.option("displayTime", "fast");
+  });
+  $("#playlist-option-displayTime-slow").click(function() {
+    myPlaylist.option("displayTime", "slow");
+  });
+  $("#playlist-option-displayTime-2000").click(function() {
+    myPlaylist.option("displayTime", 2000);
+  });
+
+  // Option: addTime
+
+  $("#playlist-option-addTime-0").click(function() {
+    myPlaylist.option("addTime", 0);
+  });
+  $("#playlist-option-addTime-fast").click(function() {
+    myPlaylist.option("addTime", "fast");
+  });
+  $("#playlist-option-addTime-slow").click(function() {
+    myPlaylist.option("addTime", "slow");
+  });
+  $("#playlist-option-addTime-2000").click(function() {
+    myPlaylist.option("addTime", 2000);
+  });
+
+  // Option: removeTime
+
+  $("#playlist-option-removeTime-0").click(function() {
+    myPlaylist.option("removeTime", 0);
+  });
+  $("#playlist-option-removeTime-fast").click(function() {
+    myPlaylist.option("removeTime", "fast");
+  });
+  $("#playlist-option-removeTime-slow").click(function() {
+    myPlaylist.option("removeTime", "slow");
+  });
+  $("#playlist-option-removeTime-2000").click(function() {
+    myPlaylist.option("removeTime", 2000);
+  });
+
+  // Option: shuffleTime
+
+  $("#playlist-option-shuffleTime-0").click(function() {
+    myPlaylist.option("shuffleTime", 0);
+  });
+  $("#playlist-option-shuffleTime-fast").click(function() {
+    myPlaylist.option("shuffleTime", "fast");
+  });
+  $("#playlist-option-shuffleTime-slow").click(function() {
+    myPlaylist.option("shuffleTime", "slow");
+  });
+  $("#playlist-option-shuffleTime-2000").click(function() {
+    myPlaylist.option("shuffleTime", 2000);
+  });
+
+  // Equivalent commands
+
+  $("#playlist-equivalent-1-a").click(function() {
+    myPlaylist.add({
+      title:"Your Face",
+      artist:"The Stark Palace",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg",
+      poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+    }, true);
+  });
+
+  $("#playlist-equivalent-1-b").click(function() {
+    myPlaylist.add({
+      title:"Your Face",
+      artist:"The Stark Palace",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg",
+      poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+    });
+    myPlaylist.play(-1);
+  });
+
+  // AVOID COMMANDS
+
+  $("#playlist-avoid-1").click(function() {
+    myPlaylist.remove(2); // Removes the 3rd item
+    myPlaylist.remove(3); // Ignored unless removeTime=0: Where it removes the 4th item, which was originally the 5th item.
+  });
+
+
+});
+
+
+//]]>
+</script>
+
 </head>
 
 
 <body  style=background-color:#FFFFFF>
-
+    
     <div id="wrapper">
 
           <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -61,8 +390,8 @@ catch (Exception $e)
                     <!-- <img src="img/logo4.png" alt="ZikOn" style="width:225px;height:100px"> -->
                     
                 </ul>
-                <?php 
-                session_start ();
+                
+                <?php
                 if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
                 ?>
                   <form class="navbar-form navbar-right" role="search" action="deconnexion.php" method="post">
@@ -99,55 +428,57 @@ catch (Exception $e)
         </nav>
 
      
-
-           
+<div id="jp_container_N" class="jp-video jp-video-240p" role="application" aria-label="media player">
+  <div class="jp-type-playlist">
+    <div id="jquery_jplayer_N" class="jp-jplayer"></div>
+    <div class="jp-gui">
+      <div class="jp-video-play">
+        <button class="jp-video-play-icon" role="button" tabindex="0">play</button>
+      </div>
+      <div class="jp-interface">
+        <div class="jp-progress">
+          <div class="jp-seek-bar">
+            <div class="jp-play-bar"></div>
+          </div>
+        </div>
+        <div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
+        <div class="jp-duration" role="timer" aria-label="duration">&nbsp;</div>
+        <div class="jp-controls-holder">
+          <div class="jp-controls">
+            <button class="jp-previous" role="button" tabindex="0">previous</button>
+            <button class="jp-play" role="button" tabindex="0">play</button>
+            <button class="jp-next" role="button" tabindex="0">next</button>
+            <button class="jp-stop" role="button" tabindex="0">stop</button>
+          </div>
+          <div class="jp-volume-controls">
+            <button class="jp-mute" role="button" tabindex="0">mute</button>
+            <button class="jp-volume-max" role="button" tabindex="0">max volume</button>
+            <div class="jp-volume-bar">
+              <div class="jp-volume-bar-value"></div>
+            </div>
+          </div>
+          <div class="jp-toggles">
+            <button class="jp-repeat" role="button" tabindex="0">repeat</button>
+            <button class="jp-shuffle" role="button" tabindex="0">shuffle</button>
+            <button class="jp-full-screen" role="button" tabindex="0">full screen</button>
+          </div>
+        </div>
+        <div class="jp-details">
+          <div class="jp-title" aria-label="title">&nbsp;</div>
+        </div>
+      </div>
+    </div>
+    <div class="jp-playlist">
+      <ul>
+        <!-- The method Playlist.displayPlaylist() uses this unordered list -->
+        <li>&nbsp;</li>
+      </ul>
+    </div>
+    </div>     
+  </div>
   <h2 id='result'></h2>
-   <table class="table table-hover" style="color:#A2A2A2;background-color:#FFFFFF;">
-    <tr>
-      <th>Titre</th> 
-      <th>Album</th>
-      <th>Artiste</th> 
-      <th>Genre</th> 
-    </tr>
- <?php
-try
-{
-    // On se connecte à MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=zikon;charset=utf8', 'root', 'root');
-}
-catch(Exception $e)
-{
-    // En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-}
-
-// Si tout va bien, on peut continuer
-$ids = join(',',$_SESSION['playlist']);  
-// On récupère tout le contenu de la table jeux_video
-
-$query = "SELECT titres_albums.id, albums.nom as anom, titres.nom as tnom , path , genres.nom as gnom , artistes.nom as artnom FROM artistes,artistes_albums,titres_albums,albums,titres,genres WHERE titres_albums.titre = titres.id and titres_albums.album = albums.id and genres.id = titres.genre and artistes.id = artistes_albums.artiste and albums.id = artistes_albums.album and titres_albums.id IN(".implode(',',$_SESSION['playlist']).")";
-$reponse = $bdd->query($query );
-
-// On affiche chaque entrée une à une
-
-while ($donnees = $reponse->fetch())
-{
-?>
-  <tr>
-         <td> <?php echo $donnees['tnom']; ?> </td>
-          <td> <?php echo $donnees['anom']; ?> </td>
-          <td> <?php echo $donnees['artnom']; ?> </td>
-          <td> <?php echo $donnees['gnom']; ?> </td>
-  </tr>
 
    
-<?php
-}
-
-$reponse->closeCursor(); // Termine le traitement de la requête
-
-?>
-</table>
 
     <!-- /#wrapper -->
     
@@ -194,6 +525,15 @@ function validate(){
     if (validateEmail(email))
       {mon_div.parentNode.insertBefore(nouveauDiv,mon_div);}
   }
+
+
+
+  // Click handlers for jPlayerPlaylist method demo
+
+  // Audio mix playlist
+
+
+
 </script>
 
 
